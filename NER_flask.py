21bcc0ecccfbabfe009bb3f7e10f5a523926ae31
kchaitanya954 <67577627+kchaitanya954@users.noreply.github.com>
2021-03-07 09:23:56 +0530

    Update NER_flask.py

--- NER_flask.py
+++ NER_flask.py
@@ -13,20 +13,20 @@ Markdown(app)
 @app.route('/')
 def index():
     return render_template('index.html')
-
+# method to render ner and to display it
 @app.route('/', methods=["GET", "POST"])
 def extract():
     if request.method == 'POST':
         if request.form['submit_button'] == 'NER Submit':
             rawtext = request.form['rawtext']
-            docx = nlp(rawtext)
-            html = displacy.render(docx, style='ent')
-            result = html
+            
+            
         elif request.form['submit_button'] == 'NER wiki Submit':
             rawtext = wikipedia.summary(request.form['rawtext'])
-            docx = nlp(rawtext)
-            html = displacy.render(docx, style='ent')
-            result = html
+
+        docx = nlp(rawtext)
+        html = displacy.render(docx, style='ent')
+        result = html
 
     return render_template('results.html', rawtext=rawtext, result=result)
 
